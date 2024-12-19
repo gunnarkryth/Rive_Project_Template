4651aa24c75701d7f36b5a5775b9119de2b5f1fa
tcaa-web <kfry@techcollege.dk>
2024-12-19 09:30:52 +0100

    cleanup

--- Rive_Frontend/scripts/rive/riveActions.js
+++
@@ -1,5 +0,0 @@
-/**
- * This file contains all the "actions" our rive animation should be capable of such as
- * the "feed" function. We can import our references to the Rive variables and trigger them, update them etc.
- * inside the functions we write here.
- */
