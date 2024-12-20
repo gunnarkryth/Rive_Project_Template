86b84f286fd0caf7347cc3e2fed228ffa58e2516
Gunski <gunnarkryth@gmail.com>
2024-12-20 09:43:16 +0100

    Update index.js

--- Rive_Backend/index.js
+++ Rive_Backend/index.js
@@ -1,7 +1,9 @@
+const { log } = require("console");
 const express = require("express");
 const app = express();
 const http = require("http");
 const PORT = 3000;
+// const PORT = 5500;
 
 // Create Express http server
 const server = http.createServer(app);
@@ -21,13 +23,38 @@ const io = new Server(server, {
 
 // Here we need to create the states for the rive animation and
 // set up an interval that updates the values
+let isSad = false;
+let hp = 100;
+
+console.log(hp);
+
+setInterval(() => {
+  if (hp > 0) {
+    if (hp < 50) {
+      isSad = true;
+    } else {
+      isSad = false;
+    }
+    hp = hp - 1;
+    console.log(hp);
+
+    io.sockets.emit("status", { sadness: isSad, health: hp });
+  }
+}, 1000);
 
 // A user connects to the server (opens a socket)
 io.sockets.on("connection", function (socket) {
   // Server recieves a ping and responds with an emit event (sends a message to all connected sockets)
-  io.sockets.emit("greet", { mesasge: "Server says hello" });
+  io.sockets.emit("greet", { message: "Server says hello" });
 
-  socket.on("hello", (data) => {
+  socket.on("feed", (data) => {
+    if (hp < 90) {
+      hp = hp + 10;
+    } else {
+      hp = 101;
+    }
     console.log("Recieved client ping: ", data);
+
+    io.sockets.emit("feeding", { message: "Client is fed" });
   });
 });
